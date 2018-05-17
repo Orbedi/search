695b6b771af188046efcd6940ad8eb9d2e071541
Orbedi <orlandobedi@outlook.es>
2018-05-17 22:02:17 +0200

    Práctica search Fundamentos de los Sistemas Inteligentes

--- search_code/run.py
+++ search_code/run.py
@@ -2,15 +2,21 @@
 
 import search
 
+#print search.breadth_first_graph_search(ab).path()
+#print search.depth_first_graph_search(ab).path()
+#print search.iterative_deepening_search(ab).path()
+#print search.depth_limited_search(ab).path()
 ab = search.GPSProblem('B', 'A', search.romania)
+print "Heuristica: ", search.estimacion_heuristica(ab).path()
+print "Coste acumulado: ", search.coste_acumulado(ab).path()
 
+ab = search.GPSProblem('C', 'F', search.romania)
+print "Heuristica: ", search.estimacion_heuristica(ab).path()
+print "Coste acumulado: ", search.coste_acumulado(ab).path()
 
-print search.breadth_first_graph_search(ab).path()
-print search.depth_first_graph_search(ab).path()
-print search.iterative_deepening_search(ab).path()
-print search.depth_limited_search(ab).path()
-print search.estimacion_heuristica(ab).path()
-print search.coste_acumulado(ab).path()
+ab = search.GPSProblem('Z', 'A', search.romania)
+print "Heuristica: ", search.estimacion_heuristica(ab).path()
+print "Coste acumulado: ", search.coste_acumulado(ab).path()
 
 #print search.astar_search(ab).path()
 
